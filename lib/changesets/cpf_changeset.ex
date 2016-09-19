6c004cbdf4a9a26b36240af70087da9de4e05f2d
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-09-19 20:50:47 -0300

    Add strictly validation for changesets

--- lib/changesets/cpf_changeset.ex
+++ lib/changesets/cpf_changeset.ex
@@ -1,13 +1,23 @@
 defmodule XBacon.Changeset.CPF do
   import Ecto.Changeset
+  import XBacon.Matchers.CPF
 
   def validate_cpf(changeset, field \\ :cpf, opts \\ []) do
     cpf = get_field(changeset, field)
 
-    case XBacon.Matchers.CPF.valid?(cpf) do
+    case valid?(cpf) do
       true -> changeset
       false ->
         add_error(changeset, field, Keyword.get(opts, :message, "CPF has invalid format."))
+    end
+  end
+
+  def validate_strictly_cpf(changeset, field \\ :cpf, opts \\ []) do
+    cpf = get_field(changeset, field, "")
+    case strictly_valid?(cpf) do
+      true -> changeset
+      false ->
+        add_error(changeset, field, Keyword.get(opts, :message, "CPF digits are not valid."))
     end
   end
 
--- test/changesets/cpf_changeset_test.exs
+++ test/changesets/cpf_changeset_test.exs
@@ -8,6 +8,7 @@ defmodule XBaconTest.Changeset.CPF do
   @valid_cpf "403.878.725-74"
   @no_dots_cpf "403878725-74"
   @invalid_cpf "534523512"
+  @invalid_verifier_cpf "403.878.725-76"
 
   test "person using a valid cpf should not get an error about it being invalid" do
     changeset = Person.changeset(%Person{}, %{cpf: @valid_cpf}) |> CPF.validate_cpf
@@ -30,7 +31,17 @@ defmodule XBaconTest.Changeset.CPF do
   test "person with missing cpf should receive an error about it" do
     changeset = Person.changeset(%Person{}, %{}) |> CPF.validate_cpf
     [cpf: {message, _}] = changeset.errors
-    assert message == "CPF has invalid format." 
+    assert message == "CPF has invalid format."
+    refute changeset.valid?
+  end
+
+  test "person with valid should pass the validation of it's digits" do
+    changeset = Person.changeset(%Person{}, %{cpf: @valid_cpf}) |> CPF.validate_strictly_cpf
+    assert changeset.valid?
+  end
+
+  test "person with invalid verification digits must not be acceptable for strictly validation" do
+    changeset = Person.changeset(%Person{}, %{cpf: @invalid_verifier_cpf}) |> CPF.validate_strictly_cpf
     refute changeset.valid?
   end
 
