c4957e24006e5bd37fea6be1d034c9d1519d9ee4
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-09-19 21:19:51 -0300

    Add documentation to cpf changeset

--- lib/changesets/cpf_changeset.ex
+++ lib/changesets/cpf_changeset.ex
@@ -2,6 +2,19 @@ defmodule XBacon.Changeset.CPF do
   import Ecto.Changeset
   import XBacon.Matchers.CPF
 
+  @doc """
+  Validates the given field on `changeset` inflecting the rules for CPF
+  Accepted formats are:
+    - xxx.xxx.xxx-xx
+    - xxxxxxxxx-xx
+  The validated field defaults to `cpf` following the convetion of function name
+  to change it you can pass a atom as second arity as the name of your field,
+  example: `validate_cpf(changeset, :cadastro_pessoa_fisica)`
+
+  To customize the error message you need to pass a keyword as thrid arity,
+  example: `validate_cpf(changeset, :cpf, message: "My message")`
+
+  """
   def validate_cpf(changeset, field \\ :cpf, opts \\ []) do
     cpf = get_field(changeset, field)
 
@@ -9,17 +22,26 @@ defmodule XBacon.Changeset.CPF do
       true -> changeset
       false ->
         add_error(changeset, field, Keyword.get(opts, :message, "CPF has invalid format."))
+      end
     end
-  end
 
-  def validate_strictly_cpf(changeset, field \\ :cpf, opts \\ []) do
-    cpf = get_field(changeset, field, "")
-    case strictly_valid?(cpf) do
-      true -> changeset
-      false ->
-        add_error(changeset, field, Keyword.get(opts, :message, "CPF digits are not valid."))
+    @doc """
+    Validates the cpf format and also validates the verification digits
+    (the last two digits) from the `changeset`
+
+    Both the field and error messages can be customizeds. The field defaults
+    to `:cpf` as the function name implies, customization can be used like:
+    `validate_strictly_cpf(changeset, :cadastro_pessoa_fisica)`
+
+    The error message can be customized using a `message:` keyword like:
+    `validate_strictly_cpf(changeset, :cpf, message: "My error message")`
+    """
+    def validate_strictly_cpf(changeset, field \\ :cpf, opts \\ []) do
+      cpf = get_field(changeset, field, "")
+      case strictly_valid?(cpf) do
+        true -> changeset
+        false ->
+          add_error(changeset, field, Keyword.get(opts, :message, "CPF digits are not valid."))
+        end
+      end
     end
-  end
-
-
-end
